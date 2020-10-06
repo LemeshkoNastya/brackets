cb056e17dee71c05d161bfa8ca2ca1407dadd2ec
LemeshkoNastya <anastasia.lemeshko23@gmail.com>
2020-10-06 12:34:52 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let answer = [];
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (bracketsConfig[j][0] == str[i]) {
+        if (answer.length != 0 && answer[answer.length - 1] == bracketsConfig[j][0] && bracketsConfig[j][1] == str[i]) {
+          answer = answer.slice(0, answer.length - 1);
+        } else {
+          answer += str[i];
+        }
+      } else if (bracketsConfig[j][1] == str[i]) {
+        if (answer[answer.length - 1] != bracketsConfig[j][0]) {
+          return false;
+        } else if (answer[answer.length - 1] == bracketsConfig[j][0]) {
+          answer = answer.slice(0, answer.length - 1);
+        }
+      }
+    }
+  }
+  if (answer.length != 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
